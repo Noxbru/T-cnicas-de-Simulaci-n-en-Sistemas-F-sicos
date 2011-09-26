5591d766a17e830df85254cb80789b7a31ef923c
Bruno <brunojimen@gmail.com>
2011-09-26 17:01:44 +0200

    cleaning a bit the N-body problem

--- Tema 1/N-bodies-Problem/program.c
+++ Tema 1/N-bodies-Problem/program.c
@@ -22,34 +22,66 @@
 #include "body.h"
 #include "system.h"
 
+void printusage(const char *name)
+{
+    fprintf(stderr,"USAGE\n");
+    fprintf(stderr,"-----\n");
+    fprintf(stderr,"%s\n",name);
+    fprintf(stderr,"OR\n");
+    fprintf(stderr,"%s -m (euler|verlet) (default verlet)\n",name);
+    exit(1);
+}
+
 int main(int argc, const char *argv[])
 {
+    int i;
+    int method=1;
+
+    for(i = 1; i < argc; i++)
+    {
+        if(!strcmp(argv[i],"-m"))
+        {
+            if(i+1==argc) printusage(argv[0]);
+            else i++;
+            if(!strcmp(argv[i],"euler"))       method = 0;
+            else if(!strcmp(argv[i],"verlet")) method = 1;
+            else printusage(argv[0]);
+        }
+        else printusage(argv[0]);
+    }
+
 
     struct system Earth_Moon,*s=&Earth_Moon;
     unsigned long int time=0;
 
-    //get_system(s);
+    Earth_Moon.n=3;
+    Earth_Moon.b=malloc(Earth_Moon.n*sizeof(struct system));
+    Earth_Moon.b[0]=(struct body){1.98892e30,{0,0,0},{0,0,0},{0,0,0},{0,0,0},1};                  //Sun if it's in the centre and fixed
+    Earth_Moon.b[1]=(struct body){5.9737e24,{1.5e11,0,0},{0,29.7389e3,0},{0,0,0},{0,0,0},0};      //Earth
+    Earth_Moon.b[2]=(struct body){7.3477e22,{1.8384e9,0,0},{0,30.802e3,0},{0,0,0},{0,0,0},0};     //Moon
+    
+    //Earth_Moon.n=2;
+    //Earth_Moon.b=malloc(Earth_Moon.n*sizeof(struct system));
+    //Earth_Moon.b[0]=(struct body){5.9737e24,{0,0,0},{0,0,0},{0,0,0},{0,0,0},1};                     //Earth
+    //Earth_Moon.b[1]=(struct body){7.3477e22,{384e6,0,0},{0,1018.865,0},{0,0,0},{0,0,0},0};          //Moon
     //print_system(Earth_Moon);
 
-    //Earth_Moon.b[0]=(struct body){1.98892e30,{0,0,0},{0,0,0},{0,0,0},{0,0,0},1};                  //Sun if it's in the centre and fixed
-    //Earth_Moon.b[1]=(struct body){5.9737e24,{1.5e11,0,0},{0,29.7389e3,0},{0,0,0},{0,0,0},0};      //Earth
-    //Earth_Moon.b[2]=(struct body){7.3477e22,{1.8384e9,0,0},{0,30.802e3,0},{0,0,0},{0,0,0},0};     //Moon
-    
-    Earth_Moon.n=2;
-    Earth_Moon.b=malloc(Earth_Moon.n*sizeof(struct system));
-    Earth_Moon.b[0]=(struct body){5.9737e24,{0,0,0},{0,0,0},{0,0,0},{0,0,0},1};                     //Earth
-    Earth_Moon.b[1]=(struct body){7.3477e22,{384e6,0,0},{0,1018.865,0},{0,0,0},{0,0,0},0};          //Moon
-    print_system(Earth_Moon);
-
-    while (time<2500000)
+    for(time = 0; time < 2500000; time+=200)
     {
-        temporal_evolution_rk(s);
-        time+=1;
-        if(time%200==0)
+        switch(method)
         {
-            printf("%lu\t%Lf\t%Lf\t%Lf\t",time,Earth_Moon.b[0].r.x,Earth_Moon.b[0].r.y,Earth_Moon.b[0].r.z);
-            printf("%Lf\t%Lf\t%Lf\n",Earth_Moon.b[1].r.x,Earth_Moon.b[1].r.y,Earth_Moon.b[1].r.z);
+            case 0:
+                for(i = 0; i < 200; i++)
+                    temporal_evolution(s,1);
+                break;
+            case 1:
+                for(i = 0; i < 200; i++)
+                    temporal_evolution_vt(s,1);
+                break;
         }
+
+        //printf("%lu\t%Lf\t%Lf\t%Lf\t",time,Earth_Moon.b[0].r.x,Earth_Moon.b[0].r.y,Earth_Moon.b[0].r.z);
+        //printf("%Lf\t%Lf\t%Lf\n",Earth_Moon.b[1].r.x,Earth_Moon.b[1].r.y,Earth_Moon.b[1].r.z);
     }
     return 0;
 }
